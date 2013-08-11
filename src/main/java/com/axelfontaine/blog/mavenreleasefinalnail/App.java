f5be38f1f787ade2aace643f2063175693d8a855
Axel Fontaine <business@axelfontaine.com>
2013-08-11 16:52:15 +0200

    Initial project structure from archetype

---
+++ src/main/java/com/axelfontaine/blog/mavenreleasefinalnail/App.java
@@ -0,0 +1,13 @@
+package com.axelfontaine.blog.mavenreleasefinalnail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
